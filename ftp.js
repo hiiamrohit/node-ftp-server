c3ed17b5254d7de338682a30e417a78096b204d7
Prabhat Khera <gizur-ess-prabhat@gizur.com>
2014-05-27 15:52:41 +0530

    changed readMe file

--- ftp.js
+++ ftp.js
@@ -5,7 +5,7 @@ Description: With this script we can create FTP server on any selected port.
 If you are using default port(21) for that case you may be have to use root privileges(sudo) 
 */
 var ftpd = require('./nodeftpd/ftpd.js');
-var server = ftpd.createServer("127.0.0.1", "./files/").listen(2121;
+var server = ftpd.createServer("127.0.0.1", "./files/").listen(2121);
 
 // this event passes in the client socket which emits further events
 // but should recommend they don't do socket operations on it
