10b3e6a15a9ba513c4d6c502279372c0dc44936d
Rohit <rohit.php5@gmail.com>
2014-07-07 15:55:06 +0530

    package.json file

--- ftp.js
+++ ftp.js
@@ -4,7 +4,7 @@ Author: Rohit Patel
 Description: With this script we can create FTP server on any selected port.
 If you are using default port(21) for that case you may be have to use root privileges(sudo) 
 */
-var ftpd = require('./nodeftpd/ftpd.js');
+var ftpd = require('ftpd.js');
 var server = ftpd.createServer("127.0.0.1", "./files/").listen(2121);
 
 // this event passes in the client socket which emits further events
